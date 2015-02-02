85dea6f8bb66d29a1737488b4b67c346daa7cacf
them.es <them.es@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 15:32:04 +0000

    Tagging version
    git-svn-id: https://plugins.svn.wordpress.org/themes-plus/tags/trunk@1080975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes-plus.php
+++ themes-plus.php
@@ -3,7 +3,7 @@
 Plugin Name: them.es Plus
 Plugin URI: http://them.es
 Description: "Short-code" your Bootstrap powered Theme and activate useful modules and features.
-Version: 1.0
+Version: 1.1
 Author: them.es
 Author URI: http://them.es
 Text Domain: themes-plus
@@ -469,5 +469,6 @@ if ( !function_exists('_log') ){
     }
   }
 }
+
 
 ?>
