cf615fda1a8284a4608a4c34549266489adf331d
them.es <them.es@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 10:26:23 +0000

    updated shortcake
    git-svn-id: https://plugins.svn.wordpress.org/themes-plus/trunk@1083692 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcake-plugin/shortcode-ui.php
+++ shortcake-plugin/shortcode-ui.php
@@ -1,23 +1,23 @@
 <?php
 /**
-* Shortcode UI
-* v0.2.0-dev
-* User Interface for adding shortcodes.
-* Fusion Engineering and community
-* http://next.fusion.net/tag/shortcode-ui/
-* shortcode-ui
-* GPL v2 or later
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*/
+ * Shortcode UI
+ * v0.2.0-dev
+ * User Interface for adding shortcodes.
+ * Fusion Engineering and community
+ * http://next.fusion.net/tag/shortcode-ui/
+ * shortcode-ui
+ * GPL v2 or later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 require_once dirname( __FILE__ ) . '/inc/class-shortcode-ui.php';
 require_once dirname( __FILE__ ) . '/inc/fields/class-shortcode-ui-fields.php';
